a6ef2a37f7a44c679753a6947c6d92e6ee2206c9
tiago <tiago.m.o.89@gmail.com>
2015-02-20 16:21:15 +0100

    added credentials fields

--- metadata.rb
+++ metadata.rb
@@ -39,3 +39,34 @@ attribute "aws/secret_key",
 :description => "Aws secret key",
 :type => 'string',
 :required => "required"
+
+attribute “google/access_key",
+:description => “Google access key",
+:type => 'string',
+:required => "required"
+
+attribute “google/secret_key",
+:description => “Google secret key",
+:type => 'string',
+:required => "required"
+
+attribute “rackspace/access_key",
+:description => “RackSpace access key",
+:type => 'string',
+:required => "required"
+
+attribute “rackspace/secret_key",
+:description => “RackSpace secret key",
+:type => 'string',
+:required => "required"
+
+attribute “azure/access_key",
+:description => “Azure access key",
+:type => 'string',
+:required => "required"
+
+attribute “azure/secret_key",
+:description => “Azure secret key",
+:type => 'string',
+:required => "required"
+
