ff8b4c33d68b2a7a7e61c42e284a5484c25b1fc1
tiago <tiago.m.o.89@gmail.com>
2015-05-21 16:40:27 +0100

    testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 bash "run_Charon" do
-  user node[:charon][:user]
+  user "#{node[:charon][:user]}"
   cwd "#{node[:charon][:home]}"
   code <<-EOH
   nohup sh Charon_mount_libs.sh > #{node[:charon][:logs_dir]}
