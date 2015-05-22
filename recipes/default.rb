45ea528b0865ed760536996fc6147bc50c3472f5
tiago <tiago.m.o.89@gmail.com>
2015-05-22 15:56:00 +0100

    testing

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,9 @@
 script "run_Charon" do
-  user #{node[:charon][:user]}
-  group #{node[:charon][:user]}
-  #user "root"
   guard_interpreter :script
-  cwd #{node[:charon][:home]}
+  user "#{node[:charon][:user]}"
+  group "#{node[:charon][:user]}"
+  #user "root"
+  cwd "#{node[:charon][:home]}"
   code <<-EOH
   nohup ./Charon_mount_libs.sh > #{node[:charon][:logs_dir]} &
   EOH
