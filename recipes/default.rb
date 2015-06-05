d05fbb84e4252519f939552f20ae8d7cb213caf6
tiago <tiago.m.o.89@gmail.com>
2015-06-05 12:02:42 +0100

    not working yet

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,6 @@ bash "run_Charon" do
   group "#{node[:charon][:user]}"
   cwd "#{node[:charon][:home]}"
   code <<-EOH
-  nohup ./Charon_mount_libs.sh > #{node[:charon][:logs_dir]} &
+  nohup ./Charon_mount.sh > #{node[:charon][:logs_dir]} &
   EOH
 end
